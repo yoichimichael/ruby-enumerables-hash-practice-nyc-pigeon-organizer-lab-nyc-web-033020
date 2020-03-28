ac3f9e4dbfac878189867bcc56fb7d29f47ee9b0
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-28 22:45:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,18 @@
 def nyc_pigeon_organizer(data)
   uniq_pigeons = {}
-  #creates list of names with duplicates
+
+  #creates AoA of names with duplicates
   pigeon_names = data[:color].reduce([]) { |memo, (key, value)| memo << value ; memo }
+
   #flattens array, removes duplicates,
   ##adds names as keys to uniq_pigeons
   pigeon_names.flatten.uniq!.map { |name| uniq_pigeons[name] = {} }
   #uniq_pigeons now has :name => {} pairs
 
-  #adds data categories to uniq_pigeons, makes new variable
-  pigeons_and_data = uniq_pigeons.each_value do |value|
-    data.each_key { |category| value[category] = [] }
-  end
+  #adds data categories to uniq_pigeons
+  uniq_pigeons.each_value do |value|
+        data.each_key { |category| value[category] = [] }
+  end 
 
   #adds colors
   pigeons_and_data.each_key do |name|
@@ -20,6 +22,6 @@ def nyc_pigeon_organizer(data)
             end
         end
   end
-  
-  pigeons_and_data
+
+  uniq_pigeons
 end
