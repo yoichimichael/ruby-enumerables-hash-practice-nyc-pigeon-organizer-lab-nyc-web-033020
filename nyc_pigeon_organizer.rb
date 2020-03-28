ee8232362093725eb9ba86c8dc8a752e602925ad
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-28 22:54:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,5 +23,14 @@ def nyc_pigeon_organizer(data)
         end
   end
 
+  #adds gender
+  uniq_pigeons.each_key do |name|
+        data[:gender].each_pair do |type, pigeons|
+            if pigeons.include?(name)
+                uniq_pigeons[name][:gender] = type.to_s
+            end
+        end
+  end
+
   uniq_pigeons
 end
